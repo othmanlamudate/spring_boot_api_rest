2ce68b3ff42395aff606b2f08c71435beb63b216
othman <lamudateothman@gmail.com>
2023-02-19 23:33:51 +0100

    add Delete Methode

--- user/src/main/kotlin/com/users/controllers/UserController.kt
+++ user/src/main/kotlin/com/users/controllers/UserController.kt
@@ -4,10 +4,7 @@ import com.users.Data.Data
 import com.users.Models.User
 import com.users.repositories.UsersRepository
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api")
@@ -33,17 +30,19 @@ class UserController(private val up:UsersRepository) {
                 @RequestParam("N",defaultValue="") N:String,@RequestParam("Pr",defaultValue="") Pr:String,
                 @RequestParam("A",defaultValue="") A:String): ResponseEntity<Boolean>
     {
-         var d=Data(up)
+        var d=Data(up)
         d.n=N
         d.e=E
         d.p=P
         d.pr=Pr
         d.a=A
-
         d.add()
-
-
-
         return  ResponseEntity.ok(true)
     }
+
+    @DeleteMapping("/delete")
+    fun deleteUser(@PathVariable id:Long){
+        this.up.deleteUser(id)
+    }
+
 }
--- user/src/main/kotlin/com/users/repositories/UsersRepository.kt
+++ user/src/main/kotlin/com/users/repositories/UsersRepository.kt
@@ -2,12 +2,19 @@ package com.users.repositories
 
 import com.users.Models.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface UsersRepository:JpaRepository<User,Int> {
   @Query("select * from user where id = ?",nativeQuery=true)
   fun search(s:String):User
+
   @Query("select * from user where email = ? and password = ?",nativeQuery=true)
   fun checkUser(e: String, p: String): User
+
+  @Modifying
+  @Query("DELETE FROM user  WHERE id=?", nativeQuery = true)
+  fun deleteUser(id:Long):Int
 
 }
